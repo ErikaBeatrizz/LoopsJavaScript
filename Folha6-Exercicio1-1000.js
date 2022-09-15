51e01410ed15552f237388032d8dcc7e935d9b6f
ErikaBeatrizz <erikabeatriz_bea@hotmail.com>
2022-09-15 09:28:24 -0300

    Exercício Lista 6

---
+++ Folha6-Exercicio1-1000.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+console.log("Valores de 1000 a 1999 que quando divididos por 11 obtemos resto = 5:  ");
+
+for (let i = 1000 ; i < 1999 ; i++) {
+    if (i % 11 == 5){
+        console.log(`Número ${i}`);
+    }
+}
+
